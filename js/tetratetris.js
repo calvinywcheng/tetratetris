b774b190c2fb55a3070ced3c847ac908d3237e0d
Calvin Cheng <calvin.cheng.cc@outlook.com>
2016-04-28 18:40:19 -0700

    Preliminary UI and input handling

--- js/tetratetris.js
+++ js/tetratetris.js
@@ -1,16 +1,116 @@
+/// <reference path="jquery.d.ts" />
 var TetraTetrisGame = (function () {
     function TetraTetrisGame() {
-        this.gameCanvas = document.getElementById("game");
-        this.gameCanvas.width = 800;
-        this.gameCanvas.height = 600;
+        this.FPS = 30;
+        this.INPUT_RATE = 5;
+        this.BLOCK_RATE = 1;
+        this.gameLoopTimerID = null;
+        this.gameCanvas = document.getElementById("game-canvas");
         this.ctx = this.gameCanvas.getContext("2d");
+        UserInput.getInstance().initHandlers(this);
     }
-    TetraTetrisGame.prototype.start = function () {
-        console.log(this.gameCanvas.toString() + "\n" + this.ctx.toString());
+    TetraTetrisGame.prototype.startGameLoop = function () {
+        var _this = this;
+        console.log("Starting game loop at + " + this.FPS + " FPS...");
+        this.gameLoopTimerID = this.gameLoopTimerID || setInterval(function () {
+            _this.update();
+            _this.render();
+        }, 1000 / this.FPS);
+    };
+    TetraTetrisGame.prototype.togglePause = function () {
+        if (this.gameLoopTimerID == null) {
+            console.log("Resuming game.");
+            this.startGameLoop();
+        }
+        else {
+            console.log("Game paused.");
+            clearInterval(this.gameLoopTimerID);
+            this.gameLoopTimerID = null;
+        }
+    };
+    TetraTetrisGame.prototype.reset = function () {
+        console.log("Resetting game...");
+        clearInterval(this.gameLoopTimerID);
+    };
+    TetraTetrisGame.prototype.update = function () {
+        console.log("updating");
+    };
+    TetraTetrisGame.prototype.render = function () {
+        console.log("rendering");
     };
     return TetraTetrisGame;
 })();
 ;
+var UserInput = (function () {
+    function UserInput() {
+        this.keysPressed = new Array();
+    }
+    UserInput.getInstance = function () {
+        if (this.instance == null) {
+            this.instance = new UserInput();
+        }
+        return this.instance;
+    };
+    UserInput.prototype.initHandlers = function (game) {
+        var _this = this;
+        $(document).ready(function () {
+            $(document).keydown(function (e) {
+                var keyCode = e.which || e.keyCode;
+                if ($.inArray(keyCode, _this.keysPressed) == -1) {
+                    _this.keysPressed.push(keyCode);
+                    console.log("Key(s) pressed: " + _this.keysPressed.map(Util.toKey));
+                }
+            });
+            $(document).keyup(function (e) {
+                var keyCode = e.which || e.keyCode;
+                var index = _this.keysPressed.indexOf(keyCode);
+                if (index != -1) {
+                    _this.keysPressed.splice(index, 1);
+                }
+                console.log("Key released: " + Util.toKey(keyCode));
+            });
+            $("#start-game").click(function () {
+                $("#start-game").prop("disabled", true);
+                $("#pause-game").prop("disabled", false);
+                game.startGameLoop();
+            });
+            $("#pause-game").click(function () {
+                game.togglePause();
+            });
+            $("#reset-game").click(function () {
+                game.reset();
+            });
+        });
+    };
+    return UserInput;
+})();
+var Util;
+(function (Util) {
+    function toKey(keyCode) {
+        switch (keyCode) {
+            case 37:
+                return "left arrow";
+            case 38:
+                return "up arrow";
+            case 39:
+                return "right arrow";
+            case 40:
+                return "down arrow";
+            case 80:
+                return "p";
+            case 32:
+                return "space";
+            case 88:
+                return "x";
+            case 90:
+                return "z";
+            case 16:
+                return "shift";
+            default:
+                return "unknown";
+        }
+    }
+    Util.toKey = toKey;
+})(Util || (Util = {}));
 var game = new TetraTetrisGame();
-game.start();
 //# sourceMappingURL=tetratetris.js.map
--- js/tetratetris.ts
+++ js/tetratetris.ts
@@ -1,19 +1,123 @@
+/// <reference path="jquery.d.ts" />
+	
 class TetraTetrisGame {
 
   private gameCanvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
+  private FPS: number = 30;
+  private INPUT_RATE: number = 5;
+  private BLOCK_RATE: number = 1;
+  private gameLoopTimerID: number = null;
 
   constructor() {
-    this.gameCanvas = <HTMLCanvasElement> document.getElementById("game");
-    this.gameCanvas.width = 800;
-    this.gameCanvas.height = 600;
+    this.gameCanvas = <HTMLCanvasElement> document.getElementById("game-canvas");
     this.ctx = this.gameCanvas.getContext("2d");
+    UserInput.getInstance().initHandlers(this);
   }
 
-  start(): void {
-    console.log(this.gameCanvas.toString() + "\n" + this.ctx.toString());
+  public startGameLoop(): void {
+    console.log("Starting game loop at + " + this.FPS + " FPS...");
+    this.gameLoopTimerID = this.gameLoopTimerID || setInterval(() => {
+      this.update();
+      this.render();
+    }, 1000 / this.FPS);
+  }
+
+  public togglePause(): void {
+    if (this.gameLoopTimerID == null) {
+      console.log("Resuming game.");
+      this.startGameLoop();
+    }
+    else {
+      console.log("Game paused.");
+      clearInterval(this.gameLoopTimerID);
+      this.gameLoopTimerID = null;
+    }
+  }
+
+  public reset(): void {
+    console.log("Resetting game...");
+    clearInterval(this.gameLoopTimerID);
+  }
+
+  public update(): void {
+    console.log("updating");
+  }
+
+  public render(): void {
+    console.log("rendering");
   }
 };
 
-var game = new TetraTetrisGame();
-game.start();
+class UserInput {
+
+  private static instance: UserInput;
+  private keysPressed: number[] = new Array<number>();
+
+  public static getInstance(): UserInput {
+    if (this.instance == null) {
+      this.instance = new UserInput();
+    }
+    return this.instance;
+  }
+
+  public initHandlers(game: TetraTetrisGame): void {
+    $(document).ready(() => {
+      $(document).keydown((e: KeyboardEvent) => {
+        let keyCode: number = e.which || e.keyCode;
+        if ($.inArray(keyCode, this.keysPressed) == -1) {
+          this.keysPressed.push(keyCode);
+          console.log("Key(s) pressed: " + this.keysPressed.map(Util.toKey));
+        }
+      });
+      $(document).keyup((e: KeyboardEvent) => {
+        let keyCode: number = e.which || e.keyCode;
+        let index = this.keysPressed.indexOf(keyCode);
+        if (index != -1) {
+          this.keysPressed.splice(index, 1);
+        }
+        console.log("Key released: " + Util.toKey(keyCode));
+      });
+      $("#start-game").click(() => {
+        $("#start-game").prop("disabled", true);
+        $("#pause-game").prop("disabled", false);
+        game.startGameLoop();
+      });
+      $("#pause-game").click(() => {
+        game.togglePause();
+      });
+      $("#reset-game").click(() => {
+        game.reset();
+      });
+    });
+  }
+}
+
+namespace Util {
+  export function toKey(keyCode: number): string {
+    switch (keyCode) {
+      case 37:
+        return "left arrow";
+      case 38:
+        return "up arrow";
+      case 39:
+        return "right arrow";
+      case 40:
+        return "down arrow";
+      case 80:
+        return "p";
+      case 32:
+        return "space";
+      case 88:
+        return "x";
+      case 90:
+        return "z";
+      case 16:
+        return "shift";
+      default:
+        return "unknown";
+    }
+  }
+}
+
+let game = new TetraTetrisGame();
